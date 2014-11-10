c75431aff136a207e890b0ad63c057d213e43db4
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2014-11-09 21:41:26 -0500

    Changed to FDNorm

---
+++ FDNorm/src/main/java/me/kahliloppenheimer/cosi128a/ServerTest.java
@@ -0,0 +1,31 @@
+package me.kahliloppenheimer.cosi128a;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+@SuppressWarnings("restriction")
+public class ServerTest {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
