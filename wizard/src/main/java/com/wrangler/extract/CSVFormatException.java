c17b274798aa941a17051215f4bed231b4953f9a
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2014-12-05 11:46:53 -0500

    Added CSVFormatException

---
+++ wizard/src/main/java/com/wrangler/extract/CSVFormatException.java
@@ -0,0 +1,14 @@
+package com.wrangler.extract;
+
+public class CSVFormatException extends Exception {
+
+	public CSVFormatException(String errorMsg) {
+		super(errorMsg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4606599952634585282L;
+
+}
