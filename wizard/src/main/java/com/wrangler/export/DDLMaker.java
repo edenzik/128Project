865890ba65a4afc432074a4f4228c95c13c3c032
Eden Zik <edenzik@Edens-MacBook-Pro.local>
2014-12-07 11:53:05 -0500

    stuff

---
+++ wizard/src/main/java/com/wrangler/export/DDLMaker.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.wrangler.export;
+
+import com.wrangler.load.Relation;
+
+/**
+ * @author edenzik
+ *
+ */
+public class DDLMaker {
+
+	/**
+	 * 
+	 */
+	public DDLMaker(Relation rel) {
+		StringBuilder statement = new StringBuilder();
+		ddl.append("fd");
+	}
+	
+	
+
+}
