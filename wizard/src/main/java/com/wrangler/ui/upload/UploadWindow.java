57fa231bf568b6e5ebf8309d16129e301e07cfa3
Eden Zik <edenzik@Edens-MacBook-Pro.local>
2014-12-08 14:57:08 -0500

    fixed upload

--- wizard/src/main/java/com/wrangler/ui/upload/UploadWindow.java
+++ wizard/src/main/java/com/wrangler/ui/upload/UploadWindow.java
@@ -11,12 +11,9 @@ import com.vaadin.ui.Upload;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Upload.ChangeEvent;
-import com.vaadin.ui.Upload.FailedEvent;
 import com.vaadin.ui.Upload.FinishedEvent;
-import com.vaadin.ui.Upload.SucceededEvent;
 import com.wrangler.ui.callback.Callback;
 import com.wrangler.ui.login.User;
-import com.wrangler.ui.wranglertool.WranglerWindow;
 
 /**
  * @author edenzik
